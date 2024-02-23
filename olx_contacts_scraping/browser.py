a9dab6954e96b278b2534ca9681c65eaf7aee0df
riguima <richard.alexsander.guima@gmail.com>
2024-02-23 13:52:31 -0300

    first stable version

--- olx_contacts_scraping/browser.py
+++ olx_contacts_scraping/browser.py
@@ -1,3 +1,6 @@
+import re
+
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -8,13 +11,59 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class Browser:
-    def __init__(self, user_data_dir, headless=True):
+    def __init__(self, headless=True):
         options = Options()
-        options.add_argument(f'--user-data-dir={user_data_dir}')
+        options.add_argument('--user-data-dir=default_user_data')
         if headless:
             options.add_argument('--headless')
             options.add_argument('--no-sandbox')
-        self.driver = Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver = Chrome(
+            service=Service(ChromeDriverManager().install()), options=options
+        )
+
+    def get_contacts_infos(self, ad_type, state, category):
+        if ad_type == 'Profissional':
+            self.driver.get(
+                f'https://www.olx.com.br/autos-e-pecas/{category}/estado-{state}?f=c&o=1'
+            )
+        elif ad_type == 'Particular':
+            self.driver.get(
+                f'https://www.olx.com.br/autos-e-pecas/{category}/estado-{state}?f=p&o=1'
+            )
+        result = {
+            'Nome': [],
+            'Telefone': [],
+        }
+        urls = self.get_all_urls()
+        for url in urls:
+            self.driver.get(url)
+            try:
+                self.find_element(
+                    'button[data-testid="button-wrapper"]'
+                ).click()
+                result['Telefone'].append(self.find_element('.sc-hknOHE').text)
+                result['Nome'].append(self.find_element('.sc-iMWBiJ').text)
+            except TimeoutException:
+                continue
+        return result
+
+    def get_all_urls(self):
+        urls = []
+        while True:
+            for ad in self.find_elements('.olx-ad-card__link-wrapper'):
+                urls.append(ad.get_attribute('href'))
+            base_url, current_page = re.findall(
+                r'(.+?)(\d+)$', self.driver.current_url, re.DOTALL
+            )[0]
+            self.driver.get(f'{base_url}{int(current_page) + 1}')
+            if (
+                base_url not in self.driver.current_url
+                or self.driver.find_elements(
+                    By.CSS_SELECTOR, '.olx-text--title-large'
+                )
+            ):
+                break
+        return urls
 
     def find_element(self, selector, element=None, wait=10):
         return WebDriverWait(element or self.driver, wait).until(
--- olx_contacts_scraping/main_window.py
+++ olx_contacts_scraping/main_window.py
@@ -1,34 +1,61 @@
+import pandas as pd
 from PySide6 import QtCore, QtWidgets
 
 from olx_contacts_scraping.browser import Browser
-from olx_contacts_scraping.consts import STATES
+from olx_contacts_scraping.consts import CATEGORIES, STATES
 
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
-        self.setFixedSize(600, 600)
+        super().__init__()
+        self.setFixedSize(600, 200)
         with open('styles.qss', 'r') as file:
             self.setStyleSheet(file.read())
 
-        self.browser = Browser('default_user_data', headless=False)
+        self.message_box = QtWidgets.QMessageBox()
+
+        self.browser = Browser(headless=False)
+
+        self.ad_type_label = QtWidgets.QLabel('Tipo de anúncio:')
+        self.ad_type_combobox = QtWidgets.QComboBox()
+        self.ad_type_combobox.addItems(['Profissional', 'Particular'])
+        self.ad_type_layout = QtWidgets.QHBoxLayout()
+        self.ad_type_layout.addWidget(self.ad_type_label)
+        self.ad_type_layout.addWidget(self.ad_type_combobox)
 
         self.state_label = QtWidgets.QLabel('Estado:')
         self.state_combobox = QtWidgets.QComboBox()
-        self.state_combobox.addItems(STATES)
+        self.state_combobox.addItems(list(STATES.values()))
         self.state_layout = QtWidgets.QHBoxLayout()
         self.state_layout.addWidget(self.state_label)
         self.state_layout.addWidget(self.state_combobox)
 
-        self.generate_spreadsheet_button = QtWidgets.QPushButton(
-            'Gerar Planilha'
-        )
-        self.generate_spreadsheet_button.clicked.connect(
-            self.generate_spreadsheet
-        )
+        self.category_label = QtWidgets.QLabel('Categoria:')
+        self.category_combobox = QtWidgets.QComboBox()
+        self.category_combobox.addItems(list(CATEGORIES.values()))
+        self.category_layout = QtWidgets.QHBoxLayout()
+        self.category_layout.addWidget(self.category_label)
+        self.category_layout.addWidget(self.category_combobox)
 
-        self.main_layout = QtWidgets.QVBoxLayout()
+        self.generate_csv_button = QtWidgets.QPushButton('Gerar CSV')
+        self.generate_csv_button.clicked.connect(self.generate_csv)
+
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+        self.main_layout.addLayout(self.ad_type_layout)
         self.main_layout.addLayout(self.state_layout)
+        self.main_layout.addLayout(self.category_layout)
+        self.main_layout.addWidget(self.generate_csv_button)
 
     @QtCore.Slot()
-    def generate_spreadsheet(self):
-        pass
+    def generate_csv(self):
+        state = list(STATES.keys())[self.state_combobox.currentIndex()]
+        category = list(CATEGORIES.keys())[
+            self.category_combobox.currentIndex()
+        ]
+        contacts_infos = self.browser.get_contacts_infos(
+            self.ad_type_combobox.currentText(), state, category
+        )
+        dataframe = pd.DataFrame.from_dict(contacts_infos)
+        dataframe.to_csv(f'result-{category}-{state}.csv', index=False)
+        self.message_box.setText('Finalizado!')
+        self.message_box.show()
